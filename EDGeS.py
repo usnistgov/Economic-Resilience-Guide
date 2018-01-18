196631f86f5abf8fe6210e17a0fc0f1f36962a86
TH Chen <tzonghao.chen@nist.gov>
2018-01-18 10:21:41 -0500

    Format disclaimer text with larger font, wrapped and centered

--- EDGeS.py
+++ EDGeS.py
@@ -25,17 +25,45 @@ from GUI.NonDBensPage import NonDBensPage
 from GUI.NonDBensUncertainties import NonDBensUncertaintiesPage
 from GUI.AnalysisInfo import AnalysisInfo
 
-#from GUI.Histograms import HistogramPage
+# from GUI.Histograms import HistogramPage
 
-from GUI.Constants import LARGE_FONT, XS_FONT
+from GUI.Constants import LARGE_FONT, NORM_FONT
 from GUI.Constants import BASE_PADDING
 
 from VertScroll import VerticalScrolledFrame
 
+
+DISCLAIMER = (
+    "This software was developed by employees of the National Institute "
+    "of Standards and Technology (NIST), an agency of the Federal "
+    "Government. Pursuant to title 17 United States Code Section 105, "
+    "works of NIST employees are not subject to copyright protection in "
+    "the United States and are considered to be in the public domain. "
+    "Permission to freely use, copy, modify, and distribute this software "
+    "and its documentation without fee is hereby granted, provided that "
+    "this notice and disclaimer of warranty appears in all copies.\n"
+    "\n"
+    "THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, "
+    "EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED "
+    "TO, ANY WARRANTY THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, "
+    "ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR "
+    "PURPOSE, AND FREEDOM FROM INFRINGEMENT, AND ANY WARRANTY THAT THE "
+    "DOCUMENTATION WILL CONFORM TO THE SOFTWARE, OR ANY WARRANTY THAT THE "
+    "SOFTWARE WILL BE ERROR FREE. IN NO EVENT SHALL NIST BE LIABLE FOR "
+    "ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT, INDIRECT, "
+    "SPECIAL OR CONSEQUENTIAL DAMAGES, ARISING OUT OF, RESULTING FROM, OR "
+    "IN ANY WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON "
+    "WARRANTY, CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS "
+    "SUSTAINED BY PERSONS OR PROPERTY OR OTHERWISE, AND WHETHER OR NOT "
+    "LOSS WAS SUSTAINED FROM, OR AROSE OUT OF THE RESULTS OF, OR USE OF, "
+    "THE SOFTWARE OR SERVICES PROVIDED HEREUNDER."
+)
+
+
 class Application(tk.Tk):
     """ Contains the actual application."""
+
     def __init__(self, *args, **kwargs):
-        #args = arguments, kwargs = key word args
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.container = VerticalScrolledFrame(self)
@@ -49,13 +77,14 @@ class Application(tk.Tk):
 
         self.frames = {}
 
-        # ====== A list of all pages (classes) used in this program.
-        # ====== Makes it possible to transition between each
+        # A list of all pages (classes) used in this program.  Makes it
+        # possible to transition between each
         frame = StartPage(self.container.interior, self)
         self.frames[StartPage] = frame
         frame.grid()
 
-        # ====== Has any text boxes select all when clicked on for ease of entering new information
+        # Has any text boxes select all when clicked on for ease of entering
+        # new information
         self.bind_class("Text", "<FocusIn>", self.selectall)
 
         self.show_frame('StartPage')
@@ -85,8 +114,10 @@ class Application(tk.Tk):
         """ Selects all items in the widget."""
         event.widget.tag_add(tk.SEL, "1.0", tk.END)
 
+
 class StartPage(tk.Frame):
     """Prompts the user to either open a file or start a new analysis"""
+
     def __init__(self, parent, controller):
         self.controller = controller
         tk.Frame.__init__(self, parent)
@@ -100,9 +131,9 @@ class StartPage(tk.Frame):
 
         photo = tk.PhotoImage(file="Edges_logo.png")
         photo = photo.subsample(8, 8)
-        #photo = tk.PhotoImage(file="CR_Logo.png")
-        #photo = photo.subsample(5, 5)
-        #photo = tk.PhotoImage(file="el_logo_small.gif")
+        # photo = tk.PhotoImage(file="CR_Logo.png")
+        # photo = photo.subsample(5, 5)
+        # photo = tk.PhotoImage(file="el_logo_small.gif")
         pic = ttk.Label(self, image=photo)
         pic.image = photo
         pic.grid(row=2)
@@ -121,35 +152,14 @@ class StartPage(tk.Frame):
         tk.Label(self, text="").grid(row=5)
 
         photo = tk.PhotoImage(file="nistident_fcenter_300ppi.png")
-        #photo = tk.PhotoImage(file="nistident_fleft_300ppi.png")
+        # photo = tk.PhotoImage(file="nistident_fleft_300ppi.png")
         photo = photo.subsample(3, 3)
         pic = ttk.Label(self, image=photo)
         pic.image = photo
         pic.grid(row=6, sticky="s")
-        #pic.grid(row=6, sticky="sw")
-        tk.Label(self, text="This software was developed by employees of the National Institute "
-                            "of Standards and Technology (NIST), an agency of the Federal \n"
-                            "Government. Pursuant to title 17 United States Code Section 105, "
-                            "works of NIST employees are not subject to copyright protection in \n"
-                            "the United States and are considered to be in the public domain. "
-                            "Permission to freely use, copy, modify, and distribute this software\n"
-                            "and its documentation without fee is hereby granted, provided that "
-                            "this notice and disclaimer of warranty appears in all copies.\n"
-                            "THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND, "
-                            "EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING,\nBUT NOT LIMITED "
-                            "TO, ANY WARRANTY THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, "
-                            "ANY IMPLIED WARRANTIES OF\nMERCHANTABILITY, FITNESS FOR A PARTICULAR "
-                            "PURPOSE, AND FREEDOM FROM INFRINGEMENT, AND ANY WARRANTY THAT THE\n"
-                            "DOCUMENTATION WILL CONFORM TO THE SOFTWARE, OR ANY WARRANTY THAT THE "
-                            "SOFTWARE WILL BE ERROR FREE. IN NO EVENT\nSHALL NIST BE LIABLE FOR "
-                            "ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT, INDIRECT, "
-                            "SPECIAL OR CONSEQUENTIAL\nDAMAGES, ARISING OUT OF, RESULTING FROM, OR "
-                            "IN ANY WAY CONNECTED WITH THIS SOFTWARE, WHETHER OR NOT BASED UPON \n"
-                            "WARRANTY, CONTRACT, TORT, OR OTHERWISE, WHETHER OR NOT INJURY WAS "
-                            "SUSTAINED BY PERSONS OR PROPERTY OR OTHERWISE,\nAND WHETHER OR NOT "
-                            "LOSS WAS SUSTAINED FROM, OR AROSE OUT OF THE RESULTS OF,\nOR USE OF, "
-                            "THE SOFTWARE OR SERVICES PROVIDED HEREUNDER.",
-                 font=XS_FONT).grid(row=7, sticky="s")
+        # pic.grid(row=6, sticky="sw")
+        ttk.Label(self, text=DISCLAIMER, justify=tk.CENTER, wraplength=500,
+                  font=NORM_FONT).grid(row=7, sticky="S")
 
     def select(self, controller):
         """Makes the OK button interact with the two given radio buttons"""
@@ -193,16 +203,16 @@ class StartPage(tk.Frame):
                         controller.frames[page] = frame
                         frame.grid(row=0, column=0, sticky="nsew")
 
-                    # === Changes these fields so that the .trace methods are envoked
-                    # ===  and the respective widgets are altered
-                    controller.frames[InfoPage].num_plans_ent.insert(tk.END,
-                                                                     controller.data_cont.num_plans-1)
+                    # Changes these fields so that the .trace methods are
+                    # envoked and the respective widgets are altered
+                    controller.frames[InfoPage].num_plans_ent.insert(
+                        tk.END, controller.data_cont.num_plans - 1)
                     for i in range(1, controller.data_cont.num_plans):
                         controller.frames[InfoPage].name_ents[i-1].delete(0, tk.END)
                         name = controller.data_cont.plan_list[i].name
                         controller.frames[InfoPage].name_ents[i-1].insert(tk.END, name)
 
-                    # ===== Global variables part of infopage
+                    # Global variables part of infopage
                     page = controller.frames[InfoPage]
                     page.name_ent.delete(0, tk.END)
                     page.name_ent.insert(tk.END, controller.data_cont.title)
